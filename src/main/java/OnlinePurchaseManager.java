7e6d69a2255be20d28f2cff5598b81e0fdaa812e
Jenny Tse <142839347+jenny-tse@users.noreply.github.com>
2024-04-15 20:39:32 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -11,30 +11,29 @@ public class OnlinePurchaseManager
   */
   public int countElectronicsByMaker(String maker)
   {
-    int count=0;
-		for(int i=0;i<purchases.size();i++) {
-			if(purchases.get(i).isElectronic()==true && purchases.get(i).getMaker()==maker) {
-				count++;
-			}
+    int count = 0; //counter 
+	for(int i=0; i<purchases.size();i++){
+		if(purchases.get(i).isElectronic()==true&&purchases.get(i).getMaker()==maker) {
+		count++;
 		}
-		return count;
+	}
+	return count;
   }
   
   /** Returns true if any pair of adjacent purchased Gizmo objects are
   * equivalent, and false otherwise, as described in part (b).
   */
-  public boolean hasAdjacentEqualPair() {
-    if(purchases.size() < 2) {
-        return false;
-    }
-    
-    for(int i = 0; i < purchases.size() - 1; i++) {
-        if(purchases.get(i).equals(purchases.get(i + 1))) {
-            return true;
-        }
-    }
-    return false;
-}
+  public boolean hasAdjacentEqualPair()
+  {
+    if(purchases.size()<2) {
+	return false;
+	}
+	for(int i = 1; i < purchases.size()-1; i++) { //starts at 1 and goes to purchases length -1
+		if(purchases.get(i).equals(purchases.get(i+1))|| purchases.get(i-1).equals(purchases.get(i))) { 
+			return true; //it will return true
+			}
+		}
+	return false; //if nothing is found it will return false
   }
   public OnlinePurchaseManager()
   {
